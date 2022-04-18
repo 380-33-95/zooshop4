0a1b17102d714654962b873303ccec7f13aa7232
boonky <3803395@gmail.com>
2022-04-18 14:05:00 +0300

    add DAO, MODEL, tovar class

---
+++ src/main/java/com/boonky/model/Tovar.java
@@ -0,0 +1,16 @@
+package com.boonky.model;
+
+public class Tovar {
+
+    private String name;
+
+    private float coast;
+
+    private int id;
+
+    private int imagetovar;
+
+    private int availability;
+
+
+}
