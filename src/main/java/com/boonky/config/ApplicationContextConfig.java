a51d0836298c6090326a887fe4ca3092e0d2f5af
boonky <3803395@gmail.com>
2022-04-23 17:48:25 +0300

    16.29

--- src/main/java/com/boonky/config/ApplicationContextConfig.java
+++ src/main/java/com/boonky/config/ApplicationContextConfig.java
@@ -16,7 +16,7 @@ public class ApplicationContextConfig{
     public InternalResourceViewResolver getViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 
-        viewResolver.setPrefix("/pages/");
+        viewResolver.setPrefix("/WEB-INF/pages/");
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
