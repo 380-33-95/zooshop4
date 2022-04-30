d2e744914568037f2ef69aff9f1c992f6087a507
boonky <3803395@gmail.com>
2022-04-30 17:42:59 +0300

    30/04/2020 1

--- src/main/java/com/boonky/controller/MyController.java
+++ src/main/java/com/boonky/controller/MyController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @Controller
 @RequestMapping("/pages")
 public class MyController {
